7f23e343058c7dc76cefb096f07ab70276f7a3f3
kimnpop <barkeum217@gmail.com>
2019-09-03 20:46:11 +0900

    Updates

---
+++ precache-manifest.13e990db60d1e7c94931c30370ac98d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487791bfe33d8cc746692d3c4ba5ca61",
+    "url": "/uplusbaseball/index.html"
+  },
+  {
+    "revision": "12f5b25b8d25b5f758be",
+    "url": "/uplusbaseball/static/css/main.86a655b7.chunk.css"
+  },
+  {
+    "revision": "2acbdbdc0077d8bf2b68",
+    "url": "/uplusbaseball/static/js/2.8090ae63.chunk.js"
+  },
+  {
+    "revision": "12f5b25b8d25b5f758be",
+    "url": "/uplusbaseball/static/js/main.566997aa.chunk.js"
+  },
+  {
+    "revision": "a084d069ef1e4f501cb4",
+    "url": "/uplusbaseball/static/js/runtime~main.a7b0eef1.js"
+  },
+  {
+    "revision": "8fe7f295264835cb4b38ff1bdd8464c4",
+    "url": "/uplusbaseball/static/media/logo.8fe7f295.png"
+  }
+]);
